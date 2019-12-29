3905caa46328a7c011762f8a96a15fbde9826899
Matthieu Zimmer <contact@matthieu-zimmer.net>
2019-12-29 04:42:38 +0100

    replace poetry by virtualenv

--- install.sh
+++ install.sh
@@ -20,25 +20,8 @@ sudo apt install -y python3-setuptools  # unfortunately required for poetry
 # Get the correct Python version
 PYTHON3_VERSION=`python3 -c 'import sys; version=sys.version_info[:3]; print("{0}{1}".format(*version))'`
 
-if [ "$PYTHON3_VERSION" -lt "36" ]; then
-    echo "\nPython version lower than 3.6! Installing 3.6...\n"
-    sudo apt install -y python3.6;
-    sudo apt install -y python3.6-venv;
-    sudo apt install -y python3.6-dev;
-    PYTHON3_CMD="python3.6";
-    PYTHON3_VERSION="36";
-    PIP_VERSION="pip3.6"
-else
-    PYTHON3_CMD="python3";
-    PIP_VERSION="pip3"
-fi
-
 # install Mininet dependencies
 sudo apt install -y openvswitch-switch cgroup-bin help2man
-# install Mininet
-cd contrib/mininet
-sudo make install PYTHON=$PYTHON3_CMD    # install the Python3 version
-cd ../..
 
 # install traffic monitors
 sudo apt install -y tcpdump ifstat
@@ -60,33 +43,26 @@ fi
 # required for traffic adjustment
 sudo apt install -y libnl-route-3-dev
 
-# Install pip locally
-export PATH+=$PATH:~/.local/bin
-wget https://bootstrap.pypa.io/get-pip.py
-$PYTHON3_CMD  get-pip.py --user
-rm get-pip.py
-
+# Install pip and virtualenv
 # Build the dc_gym
-curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | $PYTHON3_CMD
-source $HOME/.poetry/env
-poetry self:update --preview  # Update Poetry
-poetry env use $PYTHON3_CMD   # Use 3.6 for now
-# poetry cache:clear . --all  # Clear Poetry cache
-rm -rf poetry.lock            # Bugfix
-poetry update                 # Update Poetry lock dependencies
-poetry install                # Package the dc_gym
-poetry build                  # Build distribution package
+sudo apt install virtualenv
+virtualenv -p /usr/bin/python3 venv
+. venv/bin/activate
+pip install numpy pandas gym seaborn matplotlib gevent opencv-python tensorflow==1.14.0 tensorflow-probability lz4 psutil setproctitle stable-baselines
 
 # compile the traffic control
 make -C dc_gym/monitor
 make -C dc_gym/control
 
+# install Mininet
+cd contrib/mininet
+sudo make install PYTHON=$(which python)    # install the Python3 version
+cd ../..
+
 # Install the dc_gym locally
-$PIP_VERSION install --upgrade --user dist/*.whl
-
-
-# Install the latest ray build for $PYTHON3_CMD and 3
-$PIP_VERSION install --user -U https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-0.8.0.dev3-cp${PYTHON3_VERSION}-cp${PYTHON3_VERSION}m-manylinux1_x86_64.whl
+#$PIP_VERSION install --upgrade --user dist/*.whl
 
 # Install unresolved Ray runtime dependencies...
 sudo apt install -y libsm6 libxext6 libxrender-dev
+
+sudo apt install net-tools
